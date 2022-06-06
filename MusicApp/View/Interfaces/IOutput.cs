f3f0b25d168c5ac7838b017e29ad3fd48bc27184
Pawel Lorenc <pawellorencc@gmail.com>
2022-06-06 15:29:57 +0200

    Tool to display all songs from the list added

---
+++ MusicApp/View/Interfaces/IOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.View.Interfaces
+{
+    public interface IOutput<T>
+    {
+        public void DisplayAll(List<T> entity);
+    }
+}
---
+++ MusicApp/View/SongOutput.cs
@@ -0,0 +1,19 @@
+﻿using MusicApp.Data.Entities;
+using MusicApp.View.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.View
+{
+    internal class SongOutput : IOutput<Song>
+    {
+        public void DisplayAll(List<Song> entity)
+        {
+            for(int i = 0; i < entity.Count; i++)
+                Console.WriteLine("Index: " + i + " | Album: " + entity[i].AlbumName + " | Author: " + entity[i].Author + " | Length: " + entity[i].Length + " | Title: " + entity[i].Title);
+        }
+    }
+}
