2cf7495269df4e88b1ee32028a94c54b699cd3ec
Pawel Lorenc <pawellorencc@gmail.com>
2022-06-06 16:14:05 +0200

    User can add and remove songs

---
+++ MusicApp/Handler/PlaylistHandler.cs
@@ -0,0 +1,72 @@
+﻿using MusicApp.Data.DAL;
+using MusicApp.Data.Entities;
+using MusicApp.Utils;
+using MusicApp.View.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Handler
+{
+    public class PlaylistHandler
+    {
+        private readonly SongRepository _songRepo;
+        private IINputSystem _inputSystem;
+        private IOutput<Song> _output;
+        private IMenu _menu;
+        public PlaylistHandler(SongRepository songRepo, IINputSystem inputSystem, IOutput<Song> output, IMenu menu)
+        {
+            _songRepo = songRepo;
+            _inputSystem = inputSystem;
+            _output = output;
+            _menu = menu;
+        }
+
+        public void Run()
+        {
+            bool exit = false; 
+            while(!exit)
+            {
+                _output.DisplayOptions();
+                int userInput = _inputSystem.FetchIntValue("Provide a number");
+                switch(userInput)
+                {
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        AddSong();
+                        break;
+                    case 2:
+                        DeleteSong();
+                        break;
+                    case 3:
+                        DisplayAllSongs();
+                        break;
+                    case 4:
+                        SortSongs();
+                        break;
+                    case 5:
+                        DisplaySpecificSong();
+                        break;
+                    default:
+                        _menu.PrintMessage("Invalid Value");
+                        break;
+
+                }
+            }
+        }
+        public void AddSong()
+        {
+            string author = _inputSystem.FetchStringValue("Author name: ");
+            string title = _inputSystem.FetchStringValue("Title: ");
+            string album = _inputSystem.FetchStringValue("Album: ");
+            double lenght = _inputSystem.FetchDobuleValue("Length: ");
+            Song song = new Song();
+           
+        }
+
+    }
+}
---
+++ MusicApp/View/Output.cs
@@ -0,0 +1,25 @@
+﻿using MusicApp.Data.Entities;
+using MusicApp.View.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.View
+{
+    public class Output : IOutput<Song>
+    {
+        private IMenu menu = new MenuDisplay();
+        public void DisplayAll(List<Song> entity)
+        {
+            for(int i = 0; i < entity.Count; i++)
+                Console.WriteLine("Index: " + i + " | Album: " + entity[i].AlbumName + " | Author: " + entity[i].Author + " | Length: " + entity[i].Length + " | Title: " + entity[i].Title);
+        }
+
+        public void DisplayOptions()
+        {
+            menu.PrintMessage("[1] To Add song\n" + "[2] To delete song\n" + "[3] To display all songs\n" + "[4] Sort songs\n" + "[5] Display specific song\n" + "[0] To quit or go to the menu");
+        }
+    }
+}
